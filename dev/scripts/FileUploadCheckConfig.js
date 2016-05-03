7f73cc786ab45148d22195c25d3539d0ba978124
Soumyanjalee Mahapatra <mahapatra@eventbooster.de>
2016-05-03 11:05:55 +0200

    Added FileUploadFile dummy module, FileUploadChecker to check for valid files,FileUploadCheckConfig dummy module where the user can define the constraints

---
+++ dev/scripts/FileUploadCheckConfig.js
@@ -0,0 +1,13 @@
+'use strict';
+/**
+* Holds user defined constraints for file validation
+*
+*/
+var config={
+		fileSize: {
+			comparator: '>'
+			,value:''
+			,unit:''
+		}
+	,validFileTypes:[]
+};
