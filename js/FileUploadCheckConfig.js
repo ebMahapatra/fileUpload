48ba77dc126df9952ea97e2ba75c1fddfa3e5618
Soumyanjalee Mahapatra <mahapatra@eventbooster.de>
2016-05-10 18:11:37 +0200

    Corrected config object as per valid syntax

--- js/FileUploadCheckConfig.js
+++ js/FileUploadCheckConfig.js
@@ -11,30 +11,28 @@
 	}
 	,validFileTypes: []
 };*/
-let config= [
-	fileTypeValidators = [{
-		object: {
-			identifier: 'type'
-        	, name: 'file type'
-    	}
-    	, validators: [{
-            comparator: 'in'
-            , value: 'image/jpeg,image/png'
-    	}]
-	}],
-
-	fileSizeValidators = [{
-		object: {
-			identifier: 'size'
-        	, name: 'file size'
-    	}
-    	, validators: [{
-    		comparator: '<='
-        	, value: '10000000'
-    	}
-    	, {
-    		comparator: '>'
-        	, value: '200000'
-    	}]
-	}]
-];
+exports.config = [{
+	"object": {
+		"identifier": "size"
+        , "name": "file size"
+    }
+    , "validators": [{
+    	"comparator": ">"
+        , "value": 100
+    }
+    , {
+    	"comparator": "<="
+        , "value": 1000
+    }]
+}
+, {
+	"object": {
+		"identifier": "type"
+        , "name": "file type"
+    }
+    , "validators": [{
+    	"comparator": "in"
+        , "value": "image/jpeg, image/png, pdf"
+    }]
+    
+}];
