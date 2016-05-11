1e0c33b1ceaaa4657705c18f0b6a2c4d839540c3
Soumyanjalee Mahapatra <mahapatra@eventbooster.de>
2016-05-11 18:54:45 +0200

    Cosmetic change

--- test/FileUploadChecker.test.js
+++ test/FileUploadChecker.test.js
@@ -1,14 +1,15 @@
 'use strict';
 var checker=require('../js/FileUploadChecker.js');
-var config=require('../js/FileUploadCheckConfig.js');
+var configHolder=require('../js/FileUploadCheckConfig.js');
 var fileuploadFile=require('../js/FileUploadFile.js');
+var config=configHolder.config;
 
 //Test cases for validating file size
 describe('Validating file attributes', function() {
 	it('check file size greater than minimum value specified in config', function() {
 		var file = new fileuploadFile.uploadFile();
 		file.size = 150; //file size
-		file.type = 'pdf'; //file type
+		file.type = 'application/pdf'; //file type
 		var validationResult=checker.check(file,config);
 		expect(validationResult.isValidFile).toBe(true);
 	});
@@ -16,7 +17,7 @@ describe('Validating file attributes', function() {
 	it('check file size not less than minimum value specified in config', function() {
 		var file = new fileuploadFile.uploadFile();
 		file.size = 50; //file size
-		file.type = 'pdf'; //file type
+		file.type = 'application/pdf'; //file type
 		var validationResult=checker.check(file,config);
 		expect(validationResult.isValidFile).toBe(false);
 	});
@@ -24,7 +25,7 @@ describe('Validating file attributes', function() {
 	it('check file size equal to maximum value specified in config', function() {
 		var file = new fileuploadFile.uploadFile();
 		file.size = 1000; //file size
-		file.type = 'pdf'; //file type
+		file.type = 'application/pdf'; //file type
 		var validationResult=checker.check(file,config);
 		expect(validationResult.isValidFile).toBe(true);
 	});
@@ -32,15 +33,15 @@ describe('Validating file attributes', function() {
 	it('check file size <= maximum value specified in config', function() {
 		var file = new fileuploadFile.uploadFile();
 		file.size = 1001; //file size
-		file.type = 'pdf'; //file type
+		file.type = 'application/pdf'; //file type
 		var validationResult=checker.check(file,config);
 		expect(validationResult.isValidFile).toBe(false);
 	});
 
 	it('check file size equal not greater than maximum value specified in config', function() {
 		var file = new fileuploadFile.uploadFile();
-		file.size = 100000; //file size
-		file.type = 'pdf'; //file type
+		file.size = 150000; //file size
+		file.type = 'application/pdf'; //file type
 		var validationResult=checker.check(file,config);
 		expect(validationResult.isValidFile).toBe(false);
 	});
@@ -48,7 +49,7 @@ describe('Validating file attributes', function() {
 	it('check pdf as valid file type', function() {
 		var file = new fileuploadFile.uploadFile();
 		file.size = 110; //file size
-		file.type = 'pdf'; //file type
+		file.type = 'application/pdf'; //file type
 		var validationResult=checker.check(file,config);
 		expect(validationResult.isValidFile).toBe(true);
 
@@ -84,7 +85,7 @@ describe('Validating file attributes', function() {
 	it('check with invalid file size and valid file type', function() {
 		var file = new fileuploadFile.uploadFile();
 		file.size = 1000000; //file size
-		file.type = 'pdf'; //file type
+		file.type = 'application/pdf'; //file type
 		var validationResult=checker.check(file,config);
 		expect(validationResult.isValidFile).toBe(false);
 
@@ -111,7 +112,7 @@ describe('Validating file attributes', function() {
 	it('check error message for valid file type and invalid file size', function() {
 		var file = new fileuploadFile.uploadFile();
 		file.size = 1000000; //file size
-		file.type = 'pdf'; //file type
+		file.type = 'application/pdf'; //file type
 		var validationResult=checker.check(file,config);
 		expect(validationResult.errMsg[0]).toBe('Invalid file size');
 
@@ -130,7 +131,7 @@ describe('Validating file attributes', function() {
 	it('check if error exists message for valid file type and valid file size', function() {
 		var file = new fileuploadFile.uploadFile();
 		file.size = 150; //file size
-		file.type = 'pdf'; //file type
+		file.type = 'application/pdf'; //file type
 		var validationResult=checker.check(file,config);
 		expect(validationResult.errMsg.length).toBe(0);
 
