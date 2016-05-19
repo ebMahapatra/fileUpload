dc2d813abdf99949737fb83aff0832ba9c89ac0b
Soumyanjalee Mahapatra <mahapatra@eventbooster.de>
2016-05-19 14:50:14 +0200

    Added karma.conf.js

---
+++ karma.conf.js
@@ -0,0 +1,74 @@
+// Karma configuration
+// Generated on Fri May 06 2016 14:10:21 GMT+0200 (CEST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine', 'browserify'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+        "./test/FileUploadChecker.test.js"
+        , "./js/FileUploadChecker.js"
+        , "./js/FileUploadCheckConfig.js"
+        , "./js/FileUploadFile.js"
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+        'js/*.js': [ 'browserify' ],
+        'test/*.js': [ 'browserify' ]
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 3333,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
