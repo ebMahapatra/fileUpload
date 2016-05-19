56b082443f64248ad6ed2b7a25439e9c9bc27c48
Soumyanjalee Mahapatra <mahapatra@eventbooster.de>
2016-05-19 15:12:30 +0200

    Cosmetic changes

--- js/FileUploadChecker.js
+++ js/FileUploadChecker.js
@@ -4,67 +4,57 @@
 *
 */
 this.check = function(file, config) {
-	let isValidFile=true;
-	let isValidType=true;
-	let err=[];
-	let validationResult={"isValidFile" : "", "errMsg" : ""};
-	//Looping over all objects in config
-	for (let configObject of config) {
-			let isValidSize=true;
-			//Looping over validators[] in config
-			for (let validator of configObject.validators) {
-				//Validations for file size
-				if(configObject.object.identifier === 'size') {
-					//Validating for every comparator for size
-					switch(validator.comparator) {
-						case '>':
-						if (!(file.size > validator.value))
-							isValidSize=false;				
-						break;
+    let isValidFile = true;
+    let isValidType = true;
+    let err = [];
+    let validationResult = {"isValidFile" : false, "errMsg" : ""}; 
+    //Looping over all objects in config
+    for (let configObject of config) {
+            let isValidSize = true;
+            //Looping over validators[] in config
+            for (let validator of configObject.validators) {
+                //Validations for file size
+                if(configObject.object.identifier === 'size') {
+                    //Validating for every comparator for size
+                    switch(validator.comparator) {
+                        case '>':
+                        if (!(file.size > validator.value)) {
+                            isValidSize=false;              
+                        }
+                        break;
 
-	    				case '<=':
-						if (!(file.size <= validator.value))
-							isValidSize=false;		
-						break;
-    				}
-				}
-				//Validations for file type
+                        case '<=':
+                        if (!(file.size <= validator.value)) {
+                            isValidSize=false;   
+                        }
+                        break;
+                    }
+                }
+                //Validations for file type
                 if(configObject.object.identifier === 'type') {
-                	//Validating for every comparator for type
+                    //Validating for every comparator for type
                     switch(validator.comparator) {
                         case 'in':
-                        if (validator.value.indexOf(file.type) == -1)
+                        if (validator.value.indexOf(file.type) === -1)
                             isValidType=false;
                         break;
                     }
                 }
-			}
-			//Generating error if file size not valid and setting the entire file as an invalid file
-			if (!isValidSize)
-			{
-				isValidFile = false;
-				err.push('Invalid file size');
-			}
-			//Generating error if file type not valid and setting the entire file as an invalid file
-			if (!isValidType)
-			{
-				isValidFile = false;
-				err.push('Invalid file type');
-			}		
-	}
-	validationResult.isValidFile = isValidFile;
-	validationResult.errMsg = err;
-	return validationResult;
+            }
+            //Generating error if file size not valid and setting the entire file as an invalid file
+            if (!isValidSize)
+            {
+                isValidFile = false;
+                err.push('Invalid file size');
+            }
+            //Generating error if file type not valid and setting the entire file as an invalid file
+            if (!isValidType)
+            {
+                isValidFile = false;
+                err.push('Invalid file type');
+            }       
+    }
+    validationResult.isValidFile = isValidFile;
+    validationResult.errMsg = err;
+    return validationResult;
 };
-
-
-
-
-
-
-
-
-
-
-
-
