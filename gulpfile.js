8054f2e8803f37ebe9777125295131b600ca273c
Soumyanjalee Mahapatra <mahapatra@eventbooster.de>
2016-05-23 17:12:11 +0200

    Added new gulp task and made cosmetic changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,29 @@
-
-
-var gulp = require('gulp'),
+'use strict';
+let gulp = require('gulp'),
     watch = require('gulp-watch'),
     sass = require('gulp-sass'),
     minifycss = require('gulp-minify-css'),
-    gzip = require('gulp-gzip'),
     livereload = require('gulp-livereload'),
     babel = require('gulp-babel'),
     rename = require('gulp-rename'),
     del = require('del');
 
-var gzip_options = {
-    threshold: '1kb',
-    gzipOptions: {
-        level: 9
-    }
-};
+gulp.task('clean-temp', function(){
+    return del(['js/es5']);
+});
+
+gulp.task('es6-commonjs',['clean-temp'], function(){
+    return gulp.src(['js/es6/*.js'])
+        .pipe(babel())
+        .pipe(gulp.dest('js/es5'))
+        .pipe(livereload());
+});
 
 /*Compile Sass files*/
 gulp.task('sass', ['clean-stylesheets'],function() {
     return gulp.src('scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('stylesheets'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifycss())
-        .pipe(gulp.dest('stylesheets'))
-        /*.pipe(gzip(gzip_options))
-        .pipe(gulp.dest('stylesheets'))*/
         .pipe(livereload());
 });
 
@@ -34,16 +31,14 @@ gulp.task('clean-stylesheets', function(){
     return del(['stylesheets/*.css']);
 });
 
-
 /*Watch files for changes*/
 gulp.task('watch', function(){
     livereload.listen();
-   // gulp.watch('components/es2015/*.js', ['es6-commonjs']);
+    gulp.watch('js/es6/*.js', ['es6-commonjs']);
     gulp.watch('scss/*.scss', ['sass']);
 
     gulp.watch('index.html').on('change', livereload.changed);
 });
 
-//gulp.task('default',['es6-commonjs', 'sass', 'watch']);
-gulp.task('default',['sass', 'watch']);
-//gulp.watch('components/es2015/*.js');
+gulp.task('default',['es6-commonjs', 'sass', 'watch']);
+gulp.watch('js/es6/*.js');
