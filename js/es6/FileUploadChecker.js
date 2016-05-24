75e8f55af80960c2895a25bb5909948ac27f3239
Soumyanjalee Mahapatra <mahapatra@eventbooster.de>
2016-05-24 18:04:18 +0200

    Adjusted indentation

--- js/es6/FileUploadChecker.js
+++ js/es6/FileUploadChecker.js
@@ -13,71 +13,66 @@ window.ebFileUploader.fileValidator = (function () {
             this.config = config;
             this.err = [];
         }
-    //Getter property for boolean isValidFile
-    get isValidFile() {
-        let isValidFile = true;
-        let isValidType = true;
-        const err = [];
-        //Looping over config object
-        for (let configObject of this.config) {
-            let isValidSize = true;
-                //Looping over validators[] in config
-                for (let validator of configObject.validators) {
-                    //Validations for file size
-                    if(configObject.object.identifier === 'size') {
-                        //Validating for every comparator for size
-                        switch(validator.comparator) {
-                            case '>':
-                            if (!(this.file.size > validator.value)) {
-                                isValidSize = false;              
-                            }
-                            break;
+        //Getter property for boolean isValidFile
+        get isValidFile() {
+            let isValidFile = true;
+            let isValidType = true;
+            const err = [];
+            //Looping over config object
+            for (let configObject of this.config) {
+                let isValidSize = true;
+                    //Looping over validators[] in config
+                    for (let validator of configObject.validators) {
+                        //Validations for file size
+                        if(configObject.object.identifier === 'size') {
+                            //Validating for every comparator for size
+                            switch(validator.comparator) {
+                                case '>':
+                                if (!(this.file.size > validator.value)) {
+                                    isValidSize = false;              
+                                }
+                                break;
 
-                            case '<=':
-                            if (!(this.file.size <= validator.value)) {
-                                isValidSize = false;   
+                                case '<=':
+                                if (!(this.file.size <= validator.value)) {
+                                    isValidSize = false;   
+                                }
+                                break;
+                            }
+                        }
+                        //Validations for file type
+                        if(configObject.object.identifier === 'type') {
+                            //Validating for every comparator for type
+                            switch(validator.comparator) {
+                                case 'in':
+                                if (validator.value.indexOf(this.file.type) === -1)
+                                    isValidType = false;
+                                break;
                             }
-                            break;
-                        }
-                    }
-                    //Validations for file type
-                    if(configObject.object.identifier === 'type') {
-                        //Validating for every comparator for type
-                        switch(validator.comparator) {
-                            case 'in':
-                            if (validator.value.indexOf(this.file.type) === -1)
-                                isValidType = false;
-                            break;
                         }
                     }
-                }
-                //Generating error if file size not valid and setting the entire file as an invalid file
-                if (!isValidSize)
-                {
-                    isValidFile = false;
-                    this.err.push('Invalid file size');
-                }
-                //Generating error if file type not valid and setting the entire file as an invalid file
-                if (!isValidType)
-                {
-                    isValidFile = false;
-                    this.err.push('Invalid file type');
-                }  
+                    //Generating error if file size not valid and setting the entire file as an invalid file
+                    if (!isValidSize)
+                    {
+                        isValidFile = false;
+                        this.err.push('Invalid file size');
+                    }
+                    //Generating error if file type not valid and setting the entire file as an invalid file
+                    if (!isValidType)
+                    {
+                        isValidFile = false;
+                        this.err.push('Invalid file type');
+                    }  
+            }
+            return isValidFile;
         }
-        return isValidFile;
+        get errMsg() {
+            let err = this.err;
+            return err;
+        }
     }
 
-      //Getter property for String Error message
-     /* get err() {
-        //return this.err;
-        console.log(err);
-      }*/
-      get errMsg() {
-        let err = this.err;
-        return err;
-      }
-
-    }
+    //Contructs the validation result
     return function check(file,config) {
         let checker = new window.ebFileUploader.Checker(file,config);
         const validationResult = {isValidFile : false, errMsg : ""};
@@ -85,7 +80,5 @@ window.ebFileUploader.fileValidator = (function () {
         validationResult.errMsg = checker.errMsg;
         return validationResult;
     }
-    //return check;
-})(this);
 
-//window.ebFileUploader.fileValidator = fileValidator();
+})(this);
